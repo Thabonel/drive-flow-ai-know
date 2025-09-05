77284c5efdfb075c94173c55fc7bb648589bbff8
Thabonel <thabonel0@gmail.com>
2025-09-05 10:37:41 +1000

    Remove unused env vars and document required configuration

--- serve.py
+++ serve.py
@@ -1,13 +1,8 @@
 import logging
-import os
 
 from fastapi import FastAPI
 
 logging.basicConfig(level=logging.INFO)
-
-OPENAI_API_KEY = <OPENAI_API_KEY>
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = <SUPABASE_ANON_KEY>
 
 app = FastAPI()
 
