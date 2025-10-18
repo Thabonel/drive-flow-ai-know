df313218edd075dd4f148606916e0467b2ca9dae
Thabo Nel <thabonel@MacBook-Air.local>
2025-10-18 13:00:05 +1100

    security: Remove exposed encryption key from repository

--- supabase/migrations/20251018_set_microsoft_encryption_key.sql
+++
@@ -1,4 +0,0 @@
--- Set Microsoft token encryption key as a database configuration parameter
--- This key is used by the store_encrypted_microsoft_token and get_decrypted_microsoft_token functions
-
-ALTER DATABASE postgres SET app.microsoft_token_encryption_key = '3rIqniUYJtU2zqs4h8f9G8WBRbpQuT/T12uMxtvnbeM=';
